ba7ca8908cfe5f22dc6ef35666b909cc35fe7f6c
Suhyeon Kim <wisemuji@gmail.com>
2019-06-18 11:37:58 +0900

    #3 BeautyForestAgent 코드 구현 by 3604 김수현

--- BeautyForestAgent/Form1.cs
+++ BeautyForestAgent/Form1.cs
@@ -15,5 +15,37 @@ namespace BeautyForestAgent
         {
             InitializeComponent();
         }
+        private void VisibleChange(bool visible)
+        {
+            this.Visible = visible;
+            this.ntiTray.Visible = !visible;
+        }
+
+        private void BtnTray_Click(object sender, EventArgs e)
+        {
+            VisibleChange(false);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            VisibleChange(false);
+        }
+
+        private void 폼보이기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VisibleChange(true);
+        }
+
+        private void NtiTray_DoubleClick(object sender, EventArgs e)
+        {
+            VisibleChange(true);
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ntiTray.Visible = false;
+            Application.ExitThread();
+        }
     }
 }
